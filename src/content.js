1b231c2b49b84665579e9537b37649e2566e474b
darts_cloud <t.f.revolution@gmail.com>
2022-03-01 19:03:59 +0900

    v0.1.2

--- src/content.js
+++ src/content.js
@@ -1,115 +1,283 @@
 /* ========================================== 
-  
+
   【製造残】
-  
-  ・501アウト→相手がキャンセル→501アウト
-    音声が出ないはず。
 
 
- ========================================== */
-$(function(){
-    var t_round;
-    var t_player;
-    var t_legs;
-    var gameShotFlg;
+============================================ */
 
-    // https://qiita.com/taiko1/items/240eea6eb597701f83bb
-    var speak = new SpeechSynthesisUtterance();
+class Caller {
 
-    function init() {
-        var t_round = $('.input_area').attr("round");   // 0
-        var t_player = $('.input_area').attr("player"); // 0
-        t_legs = $("#legs").text();
-        gameShotFlg = false;
-        speak.rate  = 0.80;
-        speak.pitch = 1;
-        speak.lang  = 'en-GB';  //(日本語:ja-JP, アメリカ英語:en-US, イギリス英語:en-GB, 中国語:zh-CN, 韓国語:ko-KR)
+    constructor() {
+        this.init();
+        this.prev_legs = "";
+    }
+    
+    /* ======================== */
+    /* html analsys functions
+    /* ======================== */
+    getLegs() {
+        let legs = $("#legs").text();
+        if (legs == undefined || legs == "" || legs == null) {
+            legs = "";
+        }
+        return legs;
+    }
+    
+    getRound() {
+        return $('.input_area').attr("round");
+    }
+    
+    getPlayer() {
+        return $('.input_area').attr("player");
+    }
+    
+    getPoint(round, player) {
+        return $(`.score_input[round=${round}][player=${player}]`).text();
+    }
+    
+    getRequirePoint(round, player) {
+        return $(`.score_left[round=${round-1}][player=${player}]`).text();
+    }
+    
+    getLatestRound() {
+        let tdObj;
+        $("#score_body tr").each(function(i, v){
+            if(v.id == "score_list_start") {
+                return true;
+            }
+            if($(v).find(".score_left:empty").length != 0) {
+                tdObj = v;
+                return false;
+            }
+        });
+        let round = tdObj.id;
+        round = round.replace("round_", "");
+        return round;
+    }
+    
+    /* ======================== */
+    /* functions
+    /* ======================== */
+    sets(round, player) {
+        this.prev_round = round;
+        this.prev_player = player;
     }
 
-    function analsys() {
-        var legs = $("#legs").text();
-        if (legs == undefined || legs == "" || legs == null) {
-            t_legs = "";
+    init() {
+        this.prev_round = this.getRound();   // 0
+        this.prev_player = this.getPlayer(); // 0
+        this.prev_legs = this.getLegs();
+        this.gameShotFlg = false;
+    }
+    
+    isLoaded() {
+        return true;
+    }
+
+    analsys() {
+        let legs = this.getLegs();
+        //if (legs == "" || !this.loaded) {
+        if (legs == "") {
+            this.prev_legs = "";
             return;
         }
-        if (speak.voice != 'Google UK English Male') {
-            var voices = speechSynthesis.getVoices();
-            if(!voices || !voices.length){
-                return;
-            }
-            voices.some(function(v, i){
-                if(v.name == 'Google UK English Male') {
-                    speak.voice = v;
-                    return true;
-                }
-            });
+        if (!this.isLoaded()) {
+            return;
         }
-        if (t_legs != legs) {
-            // newLegs判定
-            init();
-            if($('.input_area').length > 0) {
-                // 入力できる場合
-                speak.rate  = 0.50;
-                speak.volume = 1.50;
-                speak.text = "Game On!";
-                speechSynthesis.speak(speak);
-            }
+        let round = this.getRound();
+        let player = this.getPlayer();
+        let latestRound = this.getLatestRound();
+        if (round != latestRound) {
+            return;
         }
 
-        var round = $('.input_area').attr("round");
-        var player = $('.input_area').attr("player");
-        if (t_round != round || t_player != player) {
-	        var point = $('.score_input[round="'+t_round+'"][player="'+t_player+'"]').text();
-	        // var url = chrome.runtime.getURL(point+".mp3");
-            // var music = new Audio(url);
-            // music.play();  // 再生
-            if (point >= 170) {
-                speak.rate  = 0.30;
-                speak.volume = 1.50;
+        // alert(`${this.prev_legs} != ${legs} && ${round} == 0`);
+        if (this.prev_legs != legs && round == 0) {
+            // new leg
+            this.init();
+            if($('.input_area').length > 0) {
+                // next game start
+                this.callGameOn();
             }
-            speak.text = point;
+        }
+        
+        if (this.prev_round == round && this.prev_player == player) {
+            
+            let point = this.getPoint(round, player);
+
+            if (point == 'x1' || point == 'x2' || point == 'x3') {
+                if (this.gameShotFlg) {
+                    return;
+                }
+                // Game Shot
+                this.callGameShot();
+                this.gameShotFlg = true;
+            } else {
+                this.gameShotFlg = false;
+            }
+
+        } else {
+
+	        let point = this.getPoint(this.prev_round, this.prev_player);
             if (point == "") {
-                t_round = round;
-                t_player = player;
+                this.sets(round, player);
                 return;
             }
-            if (point == 0) {
-                speak.text = "no score!";
-            }
-            speechSynthesis.speak(speak);
+	        
+            // Call Point
+            this.callPoint(point);
             
-            speak.rate  = 0.80;
-            speak.volume = 1.00;
-            
-            point = $('.score_left[round="'+(round - 1)+'"][player="'+player+'"]').text();
+            point = this.getRequirePoint(round, player);
             if (0 < point && point <= 170) {
                 if (point != 169 && point != 168 && point != 166 && point != 165 && point != 163 && point != 162 && point != 159) {
-                    speak.text = "You require " + point;
-                    speechSynthesis.speak(speak);
+                    // Call Require Point
+                    this.callYouRequire(point);
                 }
             }
-            
-            t_round = round;
-            t_player = player;
-            
-        } else {
-            // ゲーム終了判定
-            // オンライン対戦以外
-            var point = $('.score_input[round="'+round+'"][player="'+player+'"]').text();
-            if (!gameShotFlg) {
-                if (point == 'x1' || point == 'x2' || point == 'x3') {
-                    speak.rate  = 0.50;
-                    speak.volume = 1.50;
-                    speak.text = "Game Shot!";
-                    speechSynthesis.speak(speak);
-                    gameShotFlg = true;
-                    speak.rate  = 0.80;
-                    speak.volume = 1.00;
-                }
-            }
+
+            this.sets(round, player);
         }
     }
     
-    setInterval(analsys ,500);
+    callGameOn () {
+        alert("GameOn");
+    }
+    callGameShot () {
+        alert("GameOn");
+    }
+    callPoint(point) {
+        alert(point);
+    }
+    callYouRequire(point) {
+        alert(`you require ${point}`)
+    }
+}
+
+class SpeechCaller extends Caller {
+
+    constructor() {
+        super();
+        this.rate  = 0.80;
+        this.pitch = 1;
+        this.lang  = 'en-GB';  //(日本語:ja-JP, アメリカ英語:en-US, イギリス英語:en-GB, 中国語:zh-CN, 韓国語:ko-KR)
+        this.voice = null;
+    }
+
+    init() {
+        super.init();
+    }
     
+    isLoaded() {
+        // It takes a few seconds to load the voice.
+        if (this.voice != null) {
+            return true;
+        }
+        
+        let voice = null;
+        let voices = speechSynthesis.getVoices();
+        if(!voices || !voices.length){
+            return false;
+        }
+        voices.some(function(v, i){
+            if(v.name == 'Google UK English Male') {
+                voice = v;
+                return true;
+            }
+        });
+        this.voice = voice;
+        return false;
+    }
+    
+    getSpeechSynthesisUtterance() {
+        let speak = new SpeechSynthesisUtterance();
+        speak.rate  = this.rate;
+        speak.pitch = this.pitch;
+        speak.lang  = this.lang;
+        if (this.voice != null) {
+            speak.voice = this.voice;
+        }
+        return speak;
+    }
+    
+    call(speak) {
+        speechSynthesis.speak(speak);
+    }
+    
+    callGameOn() {
+        let speak = this.getSpeechSynthesisUtterance();
+        speak.rate  = 0.50;
+        speak.volume = 1.50;
+        speak.text = "Game On!";
+        this.call(speak);
+    }
+    
+    callGameShot () {
+        let speak = this.getSpeechSynthesisUtterance();
+        speak.rate  = 0.50;
+        speak.volume = 1.50;
+        speak.text = "Game Shot!";
+        this.call(speak);
+    }
+    
+    callPoint(point) {
+        let speak = this.getSpeechSynthesisUtterance();
+        if (point >= 170) {
+            speak.rate  = 0.30;
+            speak.volume = 1.50;
+        }
+        speak.text = point;
+        if (point == 0) {
+            speak.text = "no score!";
+        }
+        this.call(speak);
+    }
+    
+    callYouRequire(point) {
+        let speak = this.getSpeechSynthesisUtterance();
+        speak.text = "You require " + point;
+        this.call(speak);
+    }
+}
+
+class SoundCaller extends Caller {
+
+    constructor() {
+        super();
+    }
+
+    init() {
+        super.init();
+    }
+
+    playSound(name) {
+        // var url = chrome.runtime.getURL(`${name}.mp3`);
+        var url = chrome.runtime.getURL(`/voice/${name}.m4a`);
+        var music = new Audio(url);
+        music.play();
+    }
+
+    callGameOn() {
+	    this.playSound("GameOn");
+    }
+    
+    callGameShot () {
+	    this.playSound("GameShot");
+    }
+    
+    callPoint(point) {
+	    this.playSound(point);
+    }
+    callYouRequire(point) {
+	    this.playSound("require");
+	    this.playSound(point);
+    }
+}
+
+$(function(){
+    // var caller = new SpeechCaller();
+    var caller = new SoundCaller();
+
+	setInterval(function() {
+    	caller.analsys();
+    } ,500);
 });
